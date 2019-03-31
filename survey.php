3774c46449ace4cd4ca160ab908e61d807e4afa7
MonikaP <piechatzek@hotmail.com>
2019-03-30 21:37:07 -0700

    Added form in survey.php

--- survey.php
+++ survey.php
@@ -9,10 +9,22 @@
 <img src="logo.png" class="mx-auto d-block" alt="Responsive image">
 <div class="container">
 	<div class="row">
-		<div class="col-md-6">
-			<h1>TRU Survey</h1>
+      <div class="col-md-12 text-center">
+			
+        <h1>TRU Survey</h1> 
+        
+      </div>
+    </div>
+    <div class="row">
+          <div class="col-md-3">
+      <!-- <div class="list-survey"></div> -->
+    </div>
+    <div class="col-md-6">
 			<div class="list-survey"></div>
 		</div>
+        <div class="col-md-3">
+      <!-- <div class="list-survey"></div> -->
+    </div>
 	</div>
 </div>
 <script>
@@ -24,36 +36,41 @@
               query,
               function(data) {
                  var questions = JSON.parse(data);
-                 var form = '<form action="controller.php" method="post">'
+                 var form = '<br><form class="justify-content-center" action="controller.php" method="post">'
                     for(var i =0; i< questions.length; i++){
                  
-                      form+= '<div>';
-                      form += '<label>' + questions[i].question + '</label>';
-                      form += '</div>';
+                      form+= '<div class="p-5 mb-3 rounded bg-light">';
+                        // form += '<div class="form-group">';
+                        form += '<label class="lead form-group">' + questions[i].question + '</label>';
+                        // form += '</div>';
+                        // form += '<br>';      
+                        form+= '<div class="form-check form-group">';
+                          form += '<input class="form-check-input" type="radio" name="' + questions[i].survey_questions_id + '" value="' + questions[i].answer1 + '">';
+                          form += '<label class="form-check-label">' + questions[i].answer1 + '</label>';
+                        form += '</div>';
 
-                      form+= '<div class="custom-control custom-radio">';
-                      form += '<input class="custom-control-input" type="radio" name="' + questions[i].survey_questions_id + '" value="' + questions[i].answer1 + '">';
-                      form += '<label class="custom-control-label">' + questions[i].answer1 + '</label>';
-                      form += '</div>';
+                        form+= '<div class="form-check form-group">';
+                          form += '<input class="form-check-input" type="radio" name="' + questions[i].survey_questions_id + '" value="' + questions[i].answer2 + '"> ';
+                          form += '<label class="form-check-label">' + questions[i].answer2 + '</label>';
+                        form += '</div>';
 
-                      form+= '<div class="custom-control custom-radio">';
-                      form += '<input class="custom-control-input" type="radio" name="' + questions[i].survey_questions_id + '" value="' + questions[i].answer2 + '">';
-                      form += '<label class="custom-control-label">' + questions[i].answer2 + '</label>';
-                      form += '</div>';
+                        form+= '<div class="form-check form-group">';
+                          form += '<input class="form-check-input" type="radio" name="' + questions[i].survey_questions_id + '" value="' + questions[i].answer3 + '">';
+                          form += '<label class="form-check-label">' + questions[i].answer3 + '</label>';
+                        form += '</div>';
 
-                      form+= '<div class="custom-control custom-radio">';
-                      form += '<input class="custom-control-input" type="radio" name="' + questions[i].survey_questions_id + '" value="' + questions[i].answer3 + '">';
-                      form += '<label class="custom-control-label">' + questions[i].answer3 + '</label>';
+                        form+= '<div class="form-check form-group">';
+                          form += '<input class="form-check-input" type="radio" name="' + questions[i].survey_questions_id + '" value="' + questions[i].answer4 + '">';
+                          form += '<label class="form-check-label">' + questions[i].answer4 + '</label>';
+                        form += '</div>';
                       form += '</div>';
-
-                     form+= '<div class="custom-control custom-radio">';
-                      form += '<input class="custom-control-input" type="radio" name="' + questions[i].survey_questions_id + '" value="' + questions[i].answer4 + '">';
-                      form += '<label class="custom-control-label">' + questions[i].answer4 + '</label>';
-                      form += '</div>';
+                      // form += '<br><br><br>';
                     }
-                  form+= '<div>';
-                  form += '<input type="submit" value="Submit">';
-                      form += '</div>';
+                    form+= '<div class="col text-center">';
+                      form += '<input class="btn btn-secondary" type="submit" value="Submit">';
+                      form += '<input class="m-2 btn btn-danger" type="reset" value="Clear">';
+                    form += '</div>';
+                  form += '</form>'
                  $('.list-survey').html(form);
               });
 	});
